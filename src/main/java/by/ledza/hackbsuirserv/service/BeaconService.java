98fa7c05117692f74293aabd54df1ac42a6bb6ff
ledza228 <leshyash2003@gmail.com>
2022-04-26 13:48:46 +0300

    mock node service 2

--- src/main/java/by/ledza/hackbsuirserv/service/BeaconService.java
+++ src/main/java/by/ledza/hackbsuirserv/service/BeaconService.java
@@ -3,11 +3,13 @@ package by.ledza.hackbsuirserv.service;
 import by.ledza.hackbsuirserv.model.Beacon;
 import by.ledza.hackbsuirserv.repository.BeaconRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Qualifier("beaconProd")
 public class BeaconService implements IBeaconService{
 
     @Autowired
--- src/main/java/by/ledza/hackbsuirserv/service/NodeService.java
+++ src/main/java/by/ledza/hackbsuirserv/service/NodeService.java
@@ -10,6 +10,7 @@ import by.ledza.hackbsuirserv.repository.NodeInfoRepository;
 import by.ledza.hackbsuirserv.repository.NodeRepository;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Service
+@Qualifier("nodeProd")
 public class NodeService implements INodeService{
 
     @Autowired
